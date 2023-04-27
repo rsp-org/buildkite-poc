0781415a4b1ac8a5c24fe836ff8f4c6071c21973
Renan Polisciuc <rpolis@ext.uber.com>
2023-04-27 13:25:35 -0300

    dispatch

--- dispatch.py
+++ dispatch.py
@@ -11,19 +11,23 @@ headers = {
     "authorization": f"token {github_token}",
     "Content-Type": "application/json"
 }
-print(headers)
+print("trigger")
 r = requests.post(f"https://api.github.com/repos/{github_owner}/{github_repo}/dispatches", data=json.dumps({
     "event_type": "buildkite"
 }), headers=headers)
 
+print("wait")
 time.sleep(5)
 
 if r.status_code == 200 or r.status_code == 204:
+    print("get workflow runs")
     r = requests.get(f"https://api.github.com/repos/{github_owner}/{github_repo}/actions/runs", data=json.dumps({
         "event_type": "buildkite"
     }), headers=headers)
 
     result = r.json()
+
+    print(result)
 
     for workflow_run in result['workflow_runs']:
         id = workflow_run['id']
@@ -42,6 +46,7 @@ if r.status_code == 200 or r.status_code == 204:
                 pending = result['status'] == 'queued' or result['status'] == "in_progress"
                 
 else:
+    print("error")
     print(r.status_code)
     print(r.text)
     exit(1)
